7b7ceeafa195f8a08e02be8e2c7d38096b5e5c18
LucaSain <lucasainenco2@gmail.com>
2024-08-05 17:48:55 +0300

    emergency update 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { redirect } from "next/navigation";
 import Hero from "../components/ui/Hero";
 import Showcase from "../components/ui/Showcase";
 
 export default function Home() {
+  redirect("/tabara-urbana-astra/inscriere");
   return (
     <main>
       <Hero />
