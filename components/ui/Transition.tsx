9bd6ea190763e77d1d347a1d5a9f2672fb8b32fe
LucaSain <lucasainenco2@gmail.com>
2024-08-07 19:14:32 +0300

    Delete Unused file

--- components/ui/Transition.tsx
+++
@@ -1,7 +0,0 @@
-export default function Transition() {
-  return (
-    <div className="absolute bottom-0 blur-sm scale-[2] sm:scale-100 sm:top-0 z-10 w-screen h-max bg-cover bg-center overflow-hidden">
-      <img className="cover " src="transition.png" />
-    </div>
-  );
-}
